dac6dac30a65a2e95494e065cc981975f4355534
ma-foyez <mafoyez.web@gmail.com>
2024-10-20 17:41:36 +0600

    fixed designing

--- apps/frontend/src/components/features/hero/index.tsx
+++ apps/frontend/src/components/features/hero/index.tsx
@@ -11,11 +11,19 @@ const Hero: React.FC = () => {
       }}
     >
       <div>
-        
+        <img src="./assets/images/icons/Elements_Blue-Angle-Bottom-Left 2.png" alt="Rivercode" className='h-32 absolute bottom-0 left-0' />
+        <img src="./assets/images/icons/Elements_Blue-Angle-Bottom-Left 2.png" alt="Rivercode" className='h-32 absolute bottom-0 right-0 transform scale-x-[-1]' />
+      <div className="hidden lg:block">
+      <img 
+        src="./assets/images/file (21) 1.svg" 
+        alt="Rivercode" 
+        className="h-auto w-1/2 max-w-2xl absolute top-1/2 right-0 transform -translate-y-1/2"
+      />
       </div>
-      <div className="sl-container flex items-end h-screen pb-14">
+      </div>
+      <div className="sl-container flex items-end 2xl:items-center h-screen pb-14 2xl:pb-1">
         <div className="max-w-2xl">
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold leading-tight mb-4 sm:mb-6">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-[76px] font-bold leading-tight mb-4 sm:mb-6">
             Innovative <br /> Aviation Software <br /> for Private Jets
           </h1>
           <p className="text-sm sm:text-base md:text-lg lg:text-xl mb-6 sm:mb-8">
