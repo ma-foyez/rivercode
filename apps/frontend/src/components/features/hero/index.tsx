35eeb596a129b51340ac4dc2d5f4ece8bfd43faa
Muhammad Abul Foyez <67279274+ma-foyez@users.noreply.github.com>
2024-10-21 11:30:05 +0600

    New hero section (#1)
    * new hero designing
    
    * fixed designing
    
    * design fixed
    
    * fixed toggled nav issues
    
    * fixed build issues

--- apps/frontend/src/components/features/hero/index.tsx
+++ apps/frontend/src/components/features/hero/index.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import banner from '../../../assets/images/banner.png';
 import Button from '../../elements/button';
 
 const Hero: React.FC = () => {
   return (
     <section 
-      className="h-[420px] sm:h-screen w-full bg-cover bg-left-bottom lg:bg-center	 bg-no-repeat flex items-center"
+      className="h-[420px] sm:h-screen w-full bg-cover bg-center bg-no-repeat relative"
       style={{ 
-        backgroundImage: `url(${banner})`,
+        backgroundImage: `url(./assets/images/hero_bg.svg)`,
       }}
     >
-      <div className="sl-container w-full">
-        <div className="max-w-2xl">
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold leading-tight mb-4 sm:mb-6">
+        <div>
+            <img src="./assets/images/icons/Elements_Blue-Angle-Bottom-Left 2.png" alt="Rivercode" className='h-20 md:h-32 absolute bottom-0 left-0' />
+            <img src="./assets/images/icons/Elements_Blue-Angle-Bottom-Left 2.png" alt="Rivercode" className='h-20 md:h-32 absolute bottom-0 right-0 transform scale-x-[-1]' />
+            <div className="hidden lg:block">
+            <img 
+              src="./assets/images/file (21) 1.svg" 
+              alt="Rivercode" 
+              className="h-auto w-1/2 max-w-8xl absolute top-1/2 right-0 transform -translate-y-1/2"
+            />  
+            </div>
+        </div>
+      <div className="sl-container flex flex-col lg:flex-row md:items-center 2xl:items-center h-screen md:pb-5 2xl:pb-1 relative z-50 *:overflow-hidden">
+        <div className="hidden md:block lg:hidden -mr-12">
+              <img 
+                src="./assets/images/file (21) 1.svg" 
+                alt="Rivercode" 
+                className="w-full"
+              />  
+        </div>
+        <div className="max-w-2xl 2xl:max-w-4xl">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-[82px] font-bold leading-tight mb-4 sm:mb-6">
             Innovative <br /> Aviation Software <br /> for Private Jets
           </h1>
           <p className="text-sm sm:text-base md:text-lg lg:text-xl mb-6 sm:mb-8">
--- apps/frontend/src/components/features/layouts/index.tsx
+++ apps/frontend/src/components/features/layouts/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../header';
 import Footer from '../footer';
 import ScrollToTop from '../../elements/ScrollToTop';
 
 const Layout: React.FC = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }, [pathname]);
+
   return (
     <div className="bg-white">
       <Header />
